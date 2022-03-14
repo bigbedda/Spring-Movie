27438f1832cdb231efc9ef5692d0fa8850edf598
bigbedda <stuartjamesbedford@hotmail.com>
2022-03-14 15:22:58 +0000

    added service package and classes

--- src/main/java/com/movies/repo/MovieRepo.java
+++ src/main/java/com/movies/repo/MovieRepo.java
@@ -3,6 +3,8 @@ package com.movies.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.movies.entity.Movie;
+
 @Repository
 public interface MovieRepo extends JpaRepository<Movie, Long> {
 	
